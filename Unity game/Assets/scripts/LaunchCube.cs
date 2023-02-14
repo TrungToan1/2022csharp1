50344a06aa02a7eaf02c52314f1746ff63c84eea
TrungToan1 <34772@ma-web.nl>
2023-02-14 01:25:08 +0100

    added launching cube test

---
+++ Unity game/Assets/scripts/LaunchCube.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchCube : MonoBehaviour
+{
+    Rigidbody rig;
+    float magnitude = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+            rig = GetComponent<Rigidbody>();
+            rig.AddForce(Vector3.up * magnitude, ForceMode.Impulse);
+        }
+    }
+}
